b761c4c81f20d0be8922bb51564174119aadf1e7
Bukishi <128817249+Bukishi@users.noreply.github.com>
2025-05-02 10:45:04 -0400

    logout

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+// logout.php
+session_start();
+session_unset(); // Eliminar todas las variables de sesión
+session_destroy(); // Destruir la sesión
+header('Location: login.php'); // Redirigir al login
+exit;
